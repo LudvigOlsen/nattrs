f0685a49020cad64c64e25d8afb0faf68466d5fd
Ludvig <ludvig@clin.au.dk>
2024-08-26 19:24:47 +0200

    Adds __version__ to package

--- nattrs/__init__.py
+++ nattrs/__init__.py
@@ -1,2 +1,16 @@
-from .nested_attributes import  nested_getattr, nested_hasattr, nested_setattr, nested_mutattr
+from .nested_attributes import (
+    nested_getattr,
+    nested_hasattr,
+    nested_setattr,
+    nested_mutattr,
+)
 from .populate import populate_product
+
+
+def get_version():
+    import importlib.metadata
+
+    return importlib.metadata.version("nattrs")
+
+
+__version__ = get_version()
